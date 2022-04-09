750bbf8e25a121b88a9f9c08e76e75b8824978f2
Pasha Smolov <psmolov@nautilusdigital.com>
2022-04-09 14:13:01 -0700

    chore: add cleanup

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,6 +6,7 @@ import { IoMoon, IoMoonOutline } from 'react-icons/io5';
 
 import { Container } from './Container';
 import { setTheme } from '../store/theme/theme-actions';
+import { clearControls } from '../store/controls/controls-actions';
 
 const HeaderEl = styled.header`
   box-shadow: var(--shadow);
@@ -46,11 +47,13 @@ export const Header = () => {
 
   const  toggleTheme = () => dispatch(setTheme(theme === 'light' ? 'dark' : 'light'))
 
+  const cleanUp = () => dispatch(clearControls())
+
   return (
     <HeaderEl>
       <Container>
         <Wrapper>
-          <Title>Where is the world?</Title>
+          <Title onClick={cleanUp}>Where is the world?</Title>
           <ModeSwitcher onClick={toggleTheme}>
             {theme === 'light' ? (
               <IoMoonOutline size="14px" />
